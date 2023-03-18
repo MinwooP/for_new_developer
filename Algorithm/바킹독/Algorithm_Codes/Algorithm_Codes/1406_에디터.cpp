1eb455a6e47cdc7bca6a8f530d9952fef2a36e8f
박민우 <alsd20177@gmail.com>
2023-03-18 13:06:37 +0900

    Algorithm: Linked list

---
+++ Algorithm/바킹독/Algorithm_Codes/Algorithm_Codes/1406_에디터.cpp
@@ -0,0 +1,71 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <climits>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <map>
+#include <list>
+#include<iomanip>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	string s;
+	cin >> s;
+
+	list<char> L = {};
+	for (auto c : s) {
+		L.push_back(c);
+	}
+
+	list<char>::iterator cursor = L.end();
+
+	int M;
+	cin >> M;
+
+	while (M--) {
+		string cmd;
+		cin >> cmd;
+
+		if (cmd == "L") {
+			if (cursor != L.begin())
+				cursor--;
+		}
+		else if (cmd == "D") {
+			if (cursor != L.end())
+				cursor++;
+		}
+		else if (cmd == "B") {
+			if (cursor != L.begin()) {
+				cursor--;
+				L.erase(cursor);
+			}
+		}
+		else if (cmd == "P") {
+			char letter;
+			cin >> letter;
+
+			if (cursor != L.begin()) {
+				L.insert(cursor, letter);
+			}
+		}
+	}
+
+	for (list<char>::iterator it = L.begin(); it != L.end(); it++)
+		cout << *it;
+
+	return 0;
+}
+
---
+++ Algorithm/바킹독/연결리스트/template.md
@@ -0,0 +1,60 @@
+# [3273번_두수의 합](https://www.acmicpc.net/problem/14889)
+
+##  Category : 바킹독-
+
+-----
+
+## 🎖 내 풀이
+
++ 
+
+<br>
+
+<br>
+
+-------
+
+## 🎖 Solution
+
+### 시간 복잡도 계산 
+
+<br>
+
+### 핵심 아이디어
+
+<br>
+
+### 정답과 내 풀이 비교
+
+<br>
+
+<br>
+
+-----
+
+## 🎖 전체 코드
+
+### My code
+
+```c++
+
+```
+
+<br>
+
+<br>
+
+### After code refactoring
+
+```c++
+```
+
+<br>
+
+<br>
+
+### Solution code
+
+```c++
+```
+
